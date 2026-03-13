87492b08d218fa594b68ec482f6284703c632af2
Amr Imam <emama2337@gmail.com>
2026-03-14 00:54:40 +0200

    add quizform

--- src/components/QuizzesInstructor/QuizForm.jsx
+++ src/components/QuizzesInstructor/QuizForm.jsx
@@ -1,117 +1,108 @@
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { quizSchema } from '../../utils/validation/quizSchema'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { quizSchema } from "../../utils/validation/quizSchema"
 import Input from "../../components/Ui/Input"
 import Select from "../../components/Ui/Select"
 import Button from "../../components/Ui/Button"
-import { FaRegCircleQuestion } from 'react-icons/fa6'
+import { FaRegCircleQuestion } from "react-icons/fa6"
 
 const QuizForm = () => {
-    const {
-        register,
-        handleSubmit,
-        setValue,
-        formState: { errors },
-      } = useForm({
-        resolver: zodResolver(quizSchema),
-      })
-    
-      const onSubmit = (data) => {
-        console.log(data)
-      }
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(quizSchema),
+  })
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
   return (
-    <form className="grid gap-6"
-onSubmit={handleSubmit(onSubmit)}
->
-  <div>
-  <label className="block text-sm font-medium mb-3">
-    Quiz Title
-  </label>
-  <Input
-  placeholder="e.g., Midterm Exam - Week 5"
-  className="bg-slate-100 border-slate-300"
-  {...register("title")}
-/>
+    <form className="grid gap-6" onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label className="block text-sm font-medium mb-3">Quiz Title</label>
+        <Input
+          placeholder="e.g., Midterm Exam - Week 5"
+          className="bg-slate-100 border-slate-300"
+          {...register("title")}
+        />
 
-{errors.title && (
-  <p className="text-red-500 text-sm mt-1">
-    {errors.title.message}
-  </p>
-)}
-</div>
+        {errors.title && (
+          <p className="text-red-500 text-sm mt-1">{errors.title.message}</p>
+        )}
+      </div>
 
-<div>
-  <label className="block text-sm font-medium mb-2">
-    Select Course
-  </label>
+      <div>
+        <label className="block text-sm font-medium mb-2">Select Course</label>
 
-  <Select
-  placeholder="Choose a course..."
-  options={[
-    "Introduction to Computer Science",
-    "Data Structures and Algorithms",
-    "Web Development Basics",
-    "Database Systems",
-    "Software Engineering Principles",
-  ]}
-  className="bg-slate-100 border-slate-300"
-  onChange={(value) =>
-  setValue("course", value, { shouldValidate: true })
-}
-/>
-{errors.course && (
-  <p className="text-red-500 text-sm mt-1">
-    {errors.course.message}
-  </p>
-)}
-</div>
+        <Select
+          placeholder="Choose a course..."
+          options={[
+            "Introduction to Computer Science",
+            "Data Structures and Algorithms",
+            "Web Development Basics",
+            "Database Systems",
+            "Software Engineering Principles",
+          ]}
+          className="bg-slate-100 border-slate-300"
+          onChange={(value) =>
+            setValue("course", value, { shouldValidate: true })
+          }
+        />
+        {errors.course && (
+          <p className="text-red-500 text-sm mt-1">{errors.course.message}</p>
+        )}
+      </div>
 
-<div>
-  <label className="block text-sm font-medium mb-3">
-    Quiz Topic
-  </label>
-  <Input 
-  placeholder="e.g., Voltage Dividers and Mesh Analysis"
-  className="bg-slate-100 border-slate-300 "
-  {...register("topic")}
-  />
-  {errors.topic && (
-  <p className="text-red-500 text-sm mt-1">
-    {errors.topic.message}
-  </p>
-)}
-</div>
+      <div>
+        <label className="block text-sm font-medium mb-3">Quiz Topic</label>
+        <Input
+          placeholder="e.g., Voltage Dividers and Mesh Analysis"
+          className="bg-slate-100 border-slate-300 "
+          {...register("topic")}
+        />
+        {errors.topic && (
+          <p className="text-red-500 text-sm mt-1">{errors.topic.message}</p>
+        )}
+      </div>
 
-<div>
-  <label className="block text-sm font-medium mb-3">
-    Number of Questions
-  </label>
-   <div className="relative">
-    <Input 
-   type="number"
-  placeholder="10"
-  className="bg-slate-100 border-slate-300"
-  {...register("questions", { valueAsNumber: true })}
-  />
-  <FaRegCircleQuestion className="absolute right-4 top-1/2 -translate-y-1/2 text-indigo-600" />
-   </div>
-  {errors.questions && (
-  <p className="text-red-500 text-sm mt-1">
-    {errors.questions.message}
-  </p>
-)}
-</div>
+      <div>
+        <label className="block text-sm font-medium mb-3">
+          Number of Questions
+        </label>
+        <div className="relative">
+          <Input
+            type="number"
+            placeholder="10"
+            className="bg-slate-100 border-slate-300"
+            {...register("questions", { valueAsNumber: true })}
+          />
+          <FaRegCircleQuestion className="absolute right-4 top-1/2 -translate-y-1/2 text-indigo-600" />
+        </div>
+        {errors.questions && (
+          <p className="text-red-500 text-sm mt-1">
+            {errors.questions.message}
+          </p>
+        )}
+      </div>
 
-<div className="flex items-center justify-between">
-  <Button variant="secondary" className="px-8" type="button">
-    Cancel
-  </Button>
-  <Button className="px-10" type="submit">
-    Next
-  </Button>
-</div>
-
-</form>
+      <div className="flex items-center justify-between">
+        <Button
+          onClick={() => reset()}
+          variant="secondary"
+          className="px-8"
+          type="button"
+        >
+          Cancel
+        </Button>
+        <Button className="px-10" type="submit">
+          Next
+        </Button>
+      </div>
+    </form>
   )
 }
 
--- src/pages/dashboardInStructer/QuizzesInstructer.jsx
+++ src/pages/dashboardInStructer/QuizzesInstructer.jsx
@@ -1,36 +1,35 @@
 import React from "react"
-import { FaArrowLeft} from "react-icons/fa"
+import { FaArrowLeft } from "react-icons/fa"
 import QuizForm from "../../components/QuizzesInstructor/QuizForm"
 
-
-
 const QuizzesInstructer = () => {
-  
+  return (
+    <div className="flex flex-col p-6 gap-8">
+      <h1 className="flex gap-4 items-center text-xl font-semibold">
+        <FaArrowLeft className="text-zinc-700 hover:text-zinc-600 cursor-pointer hover:transition-colors" />
+        Create New Quiz
+      </h1>
 
-  return <div className="flex flex-col p-6 gap-8">
-    <h1 className="flex gap-4 items-center text-xl font-semibold">
-      <FaArrowLeft className="text-zinc-700 hover:text-zinc-600 cursor-pointer hover:transition-colors"/>
-     Create New Quiz
-    </h1>
+      <div className="">
+        <div className="flex justify-between text-sm mb-2">
+          <p>Step 1 of 2</p>
+          <p className="text-zinc-500">Basic Info</p>
+        </div>
 
-    <div className="">
-  <div className="flex justify-between text-sm mb-2">
-    <p>
-    Step 1 of 2
-  </p>
-  <p className="text-zinc-500">Basic Info</p>
-  </div>
+        <div className="flex gap-2 w-full h-2">
+          <div className="w-1/2 h-2 bg-indigo-600 rounded-full" />
+          <div className="w-1/2 h-2 bg-zinc-300 rounded-full" />
+        </div>
+      </div>
 
-  <div className="flex gap-2 w-full h-2">
-    <div className="w-1/2 h-2 bg-indigo-600 rounded-full"/>
-    <div className="w-1/2 h-2 bg-zinc-300 rounded-full"/>
-  </div>
-</div>
+      <p className="text-sm text-zinc-700 max-w-sm">
+        Let's start with the essentials. Provide the core details for your new
+        assessment.
+      </p>
 
-<p className="text-sm text-zinc-700 max-w-sm">Let's start with the essentials. Provide the core details for your new assessment.</p>
-
-<QuizForm/>
-  </div>
+      <QuizForm />
+    </div>
+  )
 }
 
 export default QuizzesInstructer
